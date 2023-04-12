cfc80dc7064bb63943d764a739ed83d5b849987d
princeimmanue <princeimmanue@gmail.com>
2023-04-11 23:49:24 -0700

    Displaying hello world using java program

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+// Displaying hello world using Java proramming language.
+// code by Prince Immanuel 
+
+public class helloworld {
+    public static void main(String[] args) {
+
+        System.out.println(" Hello world !");
+        
+    }
+    
+}
